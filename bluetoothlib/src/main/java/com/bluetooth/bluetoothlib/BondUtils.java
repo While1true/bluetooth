e5a6ecc1b704c34b423a5b614865e62976e2954d
Collikey <741085974@qq.com>
2018-07-19 09:51:44 +0800

    xx

--- bluetoothlib/src/main/java/com/bluetooth/bluetoothlib/BondUtils.java
+++ bluetoothlib/src/main/java/com/bluetooth/bluetoothlib/BondUtils.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 public class BondUtils {
     public static boolean startBond(BluetoothDevice btDevice) {
-        if (btDevice.getBondState() != BluetoothDevice.BOND_BONDED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                btDevice.createBond();
-            }
-            return false;
-        } else {
+        if (btDevice.getBondState() == BluetoothDevice.BOND_BONDED) {
             return true;
+
         }
+        if (btDevice.getBondState() == BluetoothDevice.BOND_NONE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            btDevice.createBond();
+        }
+        return false;
     }
 
-    public static boolean pairDevice(BluetoothDevice device,String pasword) {
+    public static boolean pairDevice(BluetoothDevice device, String pasword) {
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                if(device.setPairingConfirmation(true)&&device.setPin(pasword.getBytes()))
+                if (device.setPairingConfirmation(true) && device.setPin(pasword.getBytes()))
                     return true;
             }
         } catch (Exception e) {
